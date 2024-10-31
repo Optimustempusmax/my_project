b410b8828aa9c796fe03a925c907b1bf25af67e9
Optimus Prime <optimustempusmax@gmail.com>
2024-10-31 18:45:24 +0500

    Update hello_git.py

--- hello_git.py
+++ hello_git.py
@@ -2,3 +2,5 @@ Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print('Hello, Git')
 Hello, Git
+>>> print('Индексация - это важно')
+Индексация - это важно
