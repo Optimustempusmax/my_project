ca5e7591ca951b212f51928eecf4147e621cebf9
Optimus Prime <optimustempusmax@gmail.com>
2024-11-01 17:10:02 +0500

    Update While

--- hello_git.py
+++ hello_git.py
@@ -1,6 +1,10 @@
-Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Hello, Git')
-Hello, Git
->>> print('Индексация - это важно')
-Индексация - это важно
+print('Hello, Git!')
+print('Indexing is important!')
+
+ind = 0
+sum = 0
+
+while ind <= 9:
+        sum += ind
+        print(sum)
+        ind += 1
